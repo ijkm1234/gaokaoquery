416b55470b0de965d07327dc36a9046ed4435ba3
ijkm1234 <983256495@qq.com>
2018-03-12 22:16:22 +0800

    Revert "test1"
    This reverts commit 8fd07b7b28494f2279ec6fce94eaada00e5a076b.

--- AutoLogin/MainWindow.xaml.cs
+++ AutoLogin/MainWindow.xaml.cs
@@ -262,7 +262,9 @@ namespace AutoLogin
             var o = Regex.Replace(elementList[0], @"\s","");
             int order;
             int.TryParse(o, out order);
+            var username = Regex.Replace(elementList[1], @"\s", "");
             var id = Regex.Replace(elementList[2], @"\s", "");
+            var stu = new StuInfo() { Order = order, UserName = username, ID = id };
             return stu;
         }
         public GradeInfo ExtractGradeStr(string htmlString)
